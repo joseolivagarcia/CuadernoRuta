a792aa700840e2bd303cdcf92efec68ce0fbddb8
Jose Oliva Local <jose@joseoliva.com>
2022-08-04 01:29:27 +0200

    Relleno de todos los datos de cada pagina y mejoras en la UI

--- app/src/main/java/com/example/cuadernoruta/PagViewHolder.kt
+++ app/src/main/java/com/example/cuadernoruta/PagViewHolder.kt
@@ -1,27 +1,58 @@
 package com.example.cuadernoruta
 
 import android.view.View
+import android.widget.DatePicker
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class PagViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     val icocamper = view.findViewById<ImageView>(R.id.camper)
+
+    val fecha = view.findViewById<TextView>(R.id.tv_fecha_datos)
+    val ruta = view.findViewById<TextView>(R.id.tv_ruta_datos)
+    val km = view.findViewById<TextView>(R.id.tv_km_datos)
+
     val gasolina = view.findViewById<TextView>(R.id.tv_gasolina_datos)
     val peajes = view.findViewById<TextView>(R.id.tv_peajes_datos)
     val pernocta = view.findViewById<TextView>(R.id.tv_pernocta_datos)
 
+    val supermercados = view.findViewById<TextView>(R.id.tv_super_datos)
+    val restaurantes = view.findViewById<TextView>(R.id.tv_rtes_datos)
+    val otroscompras = view.findViewById<TextView>(R.id.tv_otros_datos)
+
+    val atracciones = view.findViewById<TextView>(R.id.tv_atracciones_datos)
+    val otrosocio = view.findViewById<TextView>(R.id.tv_otrosocio_datos)
+
+    val comentarios = view.findViewById<TextView>(R.id.tv_comentario)
+
+
+
     fun render(pagina: Pagina){
 
-        gasolina.text = pagina.gasolina.toString()
-        peajes.text = pagina.peajes.toString()
-        pernocta.text = pagina.pernocta.toString()
+        fecha.text = pagina.fecha
+        ruta.text = pagina.ruta
+        km.text = pagina.kilometros.toString() + " Km"
+
+        gasolina.text = pagina.gasolina.toString() + " €"
+        peajes.text = pagina.peajes.toString()+ " €"
+        pernocta.text = pagina.pernocta.toString()+ " €"
+
+        supermercados.text = pagina.supermercado.toString()+ " €"
+        restaurantes.text = pagina.restaurantes.toString()+ " €"
+        otroscompras.text = pagina.otrosCompras.toString()+ " €"
+
+        atracciones.text = pagina.atracciones.toString()+ " €"
+        otrosocio.text = pagina.otrosOcio.toString()+ " €"
+
+        comentarios.text = pagina.comentarios
 
         itemView.setOnClickListener{
             //hacemos clickable la tarjeta(pagina) entera, da igual en que parte pulsemos
-            Toast.makeText(gasolina.context,pagina.gasolina.toString(),Toast.LENGTH_LONG).show()
+            Toast.makeText(gasolina.context,"Has pulsado la ruta: ${pagina.ruta}",Toast.LENGTH_LONG).show()
         }
 
         //podria hacer clickable alguna parte concreta, por ejemplo el icono de la auto
--- app/src/main/java/com/example/cuadernoruta/Pagina.kt
+++ app/src/main/java/com/example/cuadernoruta/Pagina.kt
@@ -1,21 +1,22 @@
 package com.example.cuadernoruta
 
+import android.widget.DatePicker
 import java.util.*
 
 //esta data class es la clase que contiene las variables que necesitamos para rellenar cada pagina
 
 data class Pagina(
-    //val fecha: Date,
-    //val ruta: String,
-    //val kilometros: Float,
+    val fecha: String,
+    val ruta: String,
+    val kilometros: Float,
     val gasolina: Float,
     val peajes: Float,
     val pernocta: Float,
-    //val supermercado: Float,
-    //val restaurantes: Float,
-    //val otrosCompras: Float,
-    //val atracciones: Float,
-    //val otrosOcio: Float,
-    //val comentarios: String
+    val supermercado: Float,
+    val restaurantes: Float,
+    val otrosCompras: Float,
+    val atracciones: Float,
+    val otrosOcio: Float,
+    val comentarios: String
 
 )
